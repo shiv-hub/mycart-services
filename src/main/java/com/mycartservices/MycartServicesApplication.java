bd10e09e23d6b5ea2d80ce09f71eaec9d7b87b9b
shiv-hub <shivkumarmunda0097@gmail.com>
2020-12-12 17:12:19 +0530

    first commit

---
+++ src/main/java/com/mycartservices/MycartServicesApplication.java
@@ -0,0 +1,13 @@
+package com.mycartservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycartServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycartServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycart/MycartServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycartServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
